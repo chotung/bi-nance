3ec9507dff8d8008e6bc499bed646e243d813f79
Charlie Tung <choelestung@gmail.com>
2019-10-17 12:28:10 -0400

    added current price function

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ const getSymbols = async () => {
 
 // TAKES SYMBOL AND DOES DB CALL
 const findCompany = async (sym) => {
+  // Handle sanitizing user input
   // Look through db for selected stock
   let selectedComp 
   db.forEach(company => {
@@ -41,7 +42,7 @@ const findCompany = async (sym) => {
       selectedComp = company
     }
   })
-  console.log(selectedComp)
+  // console.log(selectedComp)
   // give to api call
   return selectedComp
 }
@@ -50,3 +51,13 @@ const findCompany = async (sym) => {
 // findCompany('APPLE')
 // findCompany('AAPL')
 
+const getCurrentPrice = async () => {
+  let company = await findCompany('AAPL')
+  console.log(company.symbol)
+  const res = await axios.get(`https://financialmodelingprep.com/api/v3/stock/real-time-price/${company.symbol}`)
+  // const res = await axios.get(`https://financialmodelingprep.com/api/v3/stock/real-time-price/AAPL`)
+  console.log(res.data)
+}
+
+
+getCurrentPrice()
