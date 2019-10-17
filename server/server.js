ce299411be55878187c3f389bebc178a4f634144
Charlie Tung <choelestung@gmail.com>
2019-10-17 12:16:51 -0400

    find company in database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,51 @@
 const axios = require('axios')
 
 
-const getSymbols = async (sym) => {
+// FAKE DATABASE
+
+const db = [
+  {
+    symbol: 'AAPL',
+    name: 'APPLE',
+    price: 3.33
+  },
+  {
+    symbol: 'AMZ',
+    name: 'AMAZON',
+    price: 100.69
+  },
+  {
+    symbol: 'SBUX',
+    name: 'STARBUCKS',
+    price: 100.99
+  }
+]
+
+// LIST of companies to populate the search 
+const getSymbols = async () => {
   const res = await axios.get(`https://financialmodelingprep.com/api/v3/company/stock/list`)
   console.log(res.data)
 }
 
+// getSymbols()
 
-getSymbols()
+
+// TAKES SYMBOL AND DOES DB CALL
+const findCompany = async (sym) => {
+  // Look through db for selected stock
+  let selectedComp 
+  db.forEach(company => {
+    if(company.name === sym || company.symbol === sym) {
+      // console.log(company)
+      selectedComp = company
+    }
+  })
+  console.log(selectedComp)
+  // give to api call
+  return selectedComp
+}
+
+// find company based on name or ticker symbol
+// findCompany('APPLE')
+// findCompany('AAPL')
+
