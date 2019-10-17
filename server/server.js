f2d34735959fc8da1f5df9f2e7cb2a3adcca1e71
Charlie Tung <choelestung@gmail.com>
2019-10-17 11:13:36 -0400

    added api call for list of companies

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+const axios = require('axios')
+
+
+const getSymbols = async (sym) => {
+  const res = await axios.get(`https://financialmodelingprep.com/api/v3/company/stock/list`)
+  console.log(res.data)
+}
+
+
+getSymbols()
